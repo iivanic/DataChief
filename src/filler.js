a3f1987f88779dfff2ac6ef6dd3c05669b236ba2
Igor Ivanic <igor@generator.local>
2016-05-06 12:41:00 +0200

    New and update status

--- src/filler.js
+++ src/filler.js
@@ -34,7 +34,12 @@ this.refreshFolders = function () {
         var fhtml = "";
 
         for (var j in forms) {
-            fhtml += "            <li><span style='cursor:pointer;' onclick='alert(2);' href='#'>" + forms[j] + "</span></li>"
+
+            fhtml += "            <li><span style='cursor:pointer;' onclick='alert(2);' href='#" + forms[j] + "'>" +
+                (forms[j].substring(0, 1) == 'N' ? "<img style='width:37px;height:15px;' src='../icons/new-icon-37x15.png'>" : (forms[j].substring(0, 1) == 'U' ? "<img style='width:55px;height:15px;' src='../icons/updated-icon-55x15.png'>" : "")) + forms[j].split("_")[3] + "</span></li>"
+        }
+        if (forms.length == 0) {
+            fhtml += " <li><span style='cursor:not-allowed;width:100%' href='#'>No forms in folder</span></li>";
         }
 
         html += "(" + forms.length + ")</a>" +
@@ -46,10 +51,10 @@ this.refreshFolders = function () {
     html = "";
     var recieved = helper.getFilesInDir(helper.getRecievedPath());
     for (var i in recieved) {
-        html += " <li><span style='cursor:pointer;width:100%' href='#'>" + recieved[i] + "</span";
+        html += " <li><span style='cursor:pointer;width:100%' href='#'>" + recieved[i] + "</span></li>";
     }
     if (recieved.length == 0) {
-        html += " <li><span style='cursor:not-allowed;width:100%' href='#'>No forms in folder</span";
+        html += " <li><span style='cursor:not-allowed;width:100%' href='#'>No forms in folder</span></li>";
     }
     $("#fillerTreeRecieved").html(html);
     html = "";
--- src/package.js
+++ src/package.js
@@ -9,10 +9,12 @@ this.loadPackage = function (file) {
     if (!loadedObj.publisher) {
         loadedObj.publisher = "Unknown publisher";
     }
+    var oldFiles;
     if (loadedObj.published) {
         //this package goues to publishers folder
         // create / delete publishers/NAME folder
         pp = helper.join(helper.getPublishersPath(), loadedObj.publisher);
+        oldFiles = helper.getFilesInDir(pp);
         try {
             helper.deleteFolder(pp);
         }
@@ -26,8 +28,11 @@ this.loadPackage = function (file) {
 
     helper.checkFolder(pp);
     for (var i in loadedObj.forms) {
+        var version = loadedObj.forms[i]._version;
+        var id = loadedObj.forms[i]._id;
         var content = JSON.stringify(loadedObj.forms[i], index._formEditor.saveJSONReplacer, 2);
-        var fileName = helper.join(pp, loadedObj.forms[i]._name);
+        var status = this.findFileStatus(id, version, oldFiles);
+        var fileName = helper.join(pp, status + "_" + id + "_" + version + "_" +  loadedObj.forms[i]._name);
         helper.saveTextFile(
             fileName,
             content);
@@ -37,6 +42,27 @@ this.loadPackage = function (file) {
     //we're done, delete package in inbox
     helper.deleteFile(file);
 
+}
+this.findFileStatus = function(id, version, arr)
+{
+  var ret = "N";
+  for(var i in arr)
+  {
+      var oldFileparts = arr[i].split("_");
+      if(oldFileparts[1]==id)
+      {
+          
+          if(Math.abs(version)!=Math.abs(oldFileparts[2]))
+          {
+              ret="U";
+          }
+          else 
+          {
+               ret="X";
+          }
+      }      
+  }
+    return ret;
 }
 this.loadPackages = function () {
     ipath = helper.getInboxPath();
