89a36939798d76d1ef0c760a84964a36117377b3
Igor Ivanic <igor@generator.local>
2016-05-06 13:44:05 +0200

    work on filler

--- src/filler.js
+++ src/filler.js
@@ -1,3 +1,8 @@
+var tabs;
+var tabTitle = "";
+var tabContent = "";
+var tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>";
+var tabCounter = 2;
 
 $(document).ready(function () {
     $("#btnSendRecieve").button({
@@ -10,7 +15,40 @@ $(document).ready(function () {
             filler.sendRecieve();
         });
     filler.refreshFolders();
+
+    tabs = $("#Fillertabs").tabs();
+
+
+    // close icon: removing the tab on click
+      tabs.delegate( "span.ui-icon-close", "click", function() {
+        var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+        $( "#" + panelId ).remove();
+        tabs.tabs( "refresh" );
+      });
+      
+/*
+    tabs.bind("keyup", function (event) {
+        if (event.altKey && event.keyCode === $.ui.keyCode.BACKSPACE) {
+            var panelId = tabs.find(".ui-tabs-active").remove().attr("aria-controls");
+            $("#" + panelId).remove();
+            tabs.tabs("refresh");
+        }
+    });*/
+
 });
+
+// actual addTab function: adds new tab using the input from the form above
+function addTab() {
+    var label = tabTitle.val() || "Tab " + tabCounter,
+        id = "tabs-" + tabCounter,
+        li = $(tabTemplate.replace(/#\{href\}/g, "#" + id).replace(/#\{label\}/g, label)),
+        tabContentHtml = tabContent.val() || "Tab " + tabCounter + " content.";
+
+    tabs.find(".ui-tabs-nav").append(li);
+    tabs.append("<div id='" + id + "'><p>" + tabContentHtml + "</p></div>");
+    tabs.tabs("refresh");
+    tabCounter++;
+}
 
 this.sendRecieve = function () {
 
