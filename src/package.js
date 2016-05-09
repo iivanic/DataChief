d54b203b22a6affa49dacd12bdecb3984d4df2f7
Igor Ivanic <Igor Ivanic>
2016-05-09 22:32:14 +0200

    packageimport bug

--- src/package.js
+++ src/package.js
@@ -14,9 +14,11 @@ this.loadPackage = function (file) {
         //this package goues to publishers folder
         // create / delete publishers/NAME folder
         pp = helper.join(helper.getPublishersPath(), loadedObj.publisher);
+        helper.checkFolder(pp);
         oldFiles = helper.getFilesInDir(pp);
         try {
             helper.deleteFolder(pp);
+            helper.checkFolder(pp);
         }
         catch (e)
         { }
@@ -24,44 +26,39 @@ this.loadPackage = function (file) {
     else {
         //this package goues to recieved folder
         pp = helper.getRecievedPath();
+        helper.checkFolder(pp);
     }
 
-    helper.checkFolder(pp);
     for (var i in loadedObj.forms) {
         var version = loadedObj.forms[i]._version;
         var id = loadedObj.forms[i]._id;
         var content = JSON.stringify(loadedObj.forms[i], index._formEditor.saveJSONReplacer, 2);
         var status = this.findFileStatus(id, version, oldFiles);
-        var fileName = helper.join(pp, status + "_" + id + "_" + version + "_" +  loadedObj.forms[i]._name);
+        var fileName = helper.join(pp, status + "_" + id + "_" + version + "_" + loadedObj.forms[i]._name);
         helper.saveTextFile(
             fileName,
             content);
-        //    helper.log("----Form saved as " + fileName);
+        helper.log("----Form saved as " + fileName);
 
     }
     //we're done, delete package in inbox
     helper.deleteFile(file);
 
 }
-this.findFileStatus = function(id, version, arr)
-{
-  var ret = "N";
-  for(var i in arr)
-  {
-      var oldFileparts = arr[i].split("_");
-      if(oldFileparts[1]==id)
-      {
-          
-          if(Math.abs(version)!=Math.abs(oldFileparts[2]))
-          {
-              ret="U";
-          }
-          else 
-          {
-               ret="X";
-          }
-      }      
-  }
+this.findFileStatus = function (id, version, arr) {
+    var ret = "N";
+    for (var i in arr) {
+        var oldFileparts = arr[i].split("_");
+        if (oldFileparts[1] == id) {
+
+            if (Math.abs(version) != Math.abs(oldFileparts[2])) {
+                ret = "U";
+            }
+            else {
+                ret = "X";
+            }
+        }
+    }
     return ret;
 }
 this.loadPackages = function () {
@@ -70,7 +67,7 @@ this.loadPackages = function () {
     if (files.length == 0)
         helper.log("No downloaded packages found.");
     else {
-         helper.log("Importing " + files.length + " downloaded package(s).");
+        helper.log("Importing " + files.length + " downloaded package(s).");
         for (var i in files) {
             this.loadPackage(files[i]);
         }
