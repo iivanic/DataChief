5e204a546cc28f7a18d74115526684564dd1c094
iivanic <igor_ivanic@hotmail.com>
2018-03-18 17:27:38 +0100

    geolocation work

--- src/objectmodel/currentGPSPosition.js
+++ src/objectmodel/currentGPSPosition.js
@@ -3,10 +3,13 @@ this.__proto__ = require("./fieldBase.js");
 //var helper = require("./utils.js");
 
 this._type = "currentGPSPosition";
+this.lastError=null;
+
 this._propsMeta = {
     // Since string is the default no nees to specify type
     _displayName: { group: 'Field Settings', name: 'Name', description: 'Name of the field.', showHelp: true },
-    _toolTip: { group: 'Field Settings', name: 'Tooltip', description: 'Tooltip ofr the field.', showHelp: true },
+    _toolTip: { group: 'Field Settings', name: 'Tooltip', description: 'Tooltip for the field.', showHelp: true },
+    GOOGLE_API_KEY: { group: 'Field Settings', name: 'GOOGLE API KEY', description: 'GOOGLE API KEY for geo location. Get it at https://console.developers.google.com', showHelp: true },
     _description: { group: 'Field Settings', name: 'Description', description: 'Description of the field.', showHelp: true },
     _required: { group: 'Field Validation', name: 'Required', description: 'Is this field required?', showHelp: true },
     _requiredErrorMessage: { group: 'Field Validation', name: 'Required error message', description: 'Massege whe required field is missing.', showHelp: true },
@@ -17,10 +20,14 @@ this._propsMeta = {
     _lastCumulativeId: { browsable: false },
     _form: { browsable: false },
     _parent: { browsable: false },
+    lastError: { browsable: false },
+    idprefix: { browsable: false },
+    
     _type: { browsable: false }
 }
 
 this.render = function (form, parent, placeholder, editable, user, idprefix) {
+    this.idprefix=idprefix;
     if(user=="initiator")
         user = userSettings.email;
     //  console.log("currentUserField.render()");
@@ -47,10 +54,15 @@ this.render = function (form, parent, placeholder, editable, user, idprefix) {
         (this._required ? "required" : "") + " readonly style='width:100%' type='text'  id='" + idprefix + "_" + this.id + "' class='datachiefField currentGPSPositionFiledMarker' value='" + this.position2Text(this._value) + "' />";
     // if (this._value == "" && !disabled) {
     ret += "<p class='help-block'></p></div></div>";
+    ret += "<span id='" + idprefix + "_" + this.id + "_error' class='red'>" + (this.lastError? displayError() :"" )  + "</span>";
     // }
     ret += "</div>";
     return ret;
 };
+this.displayError = function()
+{
+    return "Geo location needs GPS sensor on your device or Internet Connection and GOOGLE_API_KEY set. Error: " + this.lastError.message 
+}
 this.ctor = function () {
     this._children = new Array();
     this.__proto__.ctor();
@@ -64,6 +76,9 @@ this.ctor = function () {
     this._requiredErrorMessage = "This field is required.";
     this._form = null;
     this._parent = null;
+    this.lastError=null;
+    this.GOOGLE_API_KEY = "AIzaSyDbpAr6vFKM4T0lwNlD3v9r43ieTyqn9rI";
+this.idprefix = "";
 }
 this.findField = function (idwithprefix) {
     //   console.log("currentUserField.findField(" + idwithprefix + ")");
@@ -77,16 +92,25 @@ this.findField = function (idwithprefix) {
 }
 this.getPosition = function()
 {
-    navigator.geolocation.getCurrentPosition(this.readPosition, this.handle_error,
+    const electron = window.require('electron'); 
+    const remote = electron.remote; //... console.log(remote.process.env["TZ"]);
+    remote.process.env.GOOGLE_API_KEY = this.GOOGLE_API_KEY;
+    this.lastError=null;
+    navigator.geolocation.getCurrentPosition(this.readPosition, 
+        function(err){
+            helper.log("currentGPSPosition error: code=" + err.code + ", message=" + err.message);
+            this.lastError=err;
+            $("#" + this.idprefix + "_" + this.id + "_error").html(this.displayError());
+        }.bind(this) 
+        ,
         {
-            maximumAge: 75000,
+       //     maximumAge: 75000,
             enableHighAccuracy: true
         });
 }
-this.handle_error = function(err)
-{
-    helper.log("currentGPSPosition error: code=" + err.code + ", message=" + err.message);
-}
+
+
+
 this.readPosition = function(position)
 {
     alert("position");
